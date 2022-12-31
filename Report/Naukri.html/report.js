cd5a2bab4b419463f8b24512dd8acd10fd158288
Maniarchun <maniarchun@gmail.com>
2022-12-31 19:47:08 +0530

    3

---
+++ Report/Naukri.html/report.js
@@ -0,0 +1,132 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/ELCOT/eclipse-workspace/Naukri/src/test/java/com/feature/Naukri.feature");
+formatter.feature({
+  "name": "Upload Resume into Naukri",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "LoginPage",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "user Launch The Application Url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepDefinition.user_Launch_The_Application_Url()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Click The Login",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.user_Click_The_Login()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Enter The User Name",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.user_Enter_The_User_Name()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Enther The Password",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.user_Enther_The_Password()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Click The To Login",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_Click_The_To_Login()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Click The Profile Option Icon",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.user_Click_The_Profile_Option_Icon()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Click The Update Profile",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_Click_The_Update_Profile()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Delete The Resume",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.user_Delete_The_Resume()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Upload The Resume",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.user_Upload_The_Resume()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Refresh The Page",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.user_Refresh_The_Page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Again Click The Profile Option Icon",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "StepDefinition.user_Again_Click_The_Profile_Option_Icon()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user Click The Logout",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_Click_The_Logout()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
